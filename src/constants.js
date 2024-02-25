7928798c1e01abb2f8dbbabb8015eb8e92f89743
Achouak Cherif <65472852+Achwak28@users.noreply.github.com>
2024-02-25 20:24:32 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -6,37 +6,37 @@ const TEAM_MEMBERS = [
   },
   {
     id: "m2",
-    image: "/src/assets/about-us/kei.png",
-    description: "/src/assets/about-us/kei_description.png",
+    image: "/assets/about-us/kei.png",
+    description: "/assets/about-us/kei_description.png",
   },
   {
     id: "m3",
-    image: "/src/assets/about-us/mike.png",
-    description: "/src/assets/about-us/mike_description.png",
+    image: "/assets/about-us/mike.png",
+    description: "/assets/about-us/mike_description.png",
   },
   {
     id: "m4",
-    image: "/src/assets/about-us/lara.png",
-    description: "/src/assets/about-us/lara_description.png",
+    image: "/assets/about-us/lara.png",
+    description: "/assets/about-us/lara_description.png",
   },
 ];
 const features = [
   {
     id: "f1",
     title:"Commitment to Creativity",
-    image: "/src/assets/about-us/feature-icon-1.png",
+    image: "/assets/about-us/feature-icon-1.png",
     text: "Commitment to Creativity",
   },
   {
     id: "f2",
     title:"Client Satisfaction",
-    image: "/src/assets/about-us/feature-icon-2.png",
+    image: "/assets/about-us/feature-icon-2.png",
     text: "Client Satisfaction",
   },
   {
     id: "f3",
     title:"Digital Trends",
-    image: "/src/assets/about-us/feature-icon-3.png",
+    image: "/assets/about-us/feature-icon-3.png",
     text: "Digital Trends",
   },
   
@@ -46,19 +46,19 @@ const services = [
   {
     id: "s1",
     title:"A Decade of Expertise",
-    image: "/src/assets/about-us/decade.svg",
+    image: "/assets/about-us/decade.svg",
     text: "A Decade of Expertise",
   },
   {
     id: "s2",
     title:"Innovative Design",
-    image: "/src/assets/about-us/clean.svg",
+    image: "/assets/about-us/clean.svg",
     text: "Innovative Design",
   },
   {
     id: "s3",
     title:"Client-Centric Approach",
-    image: "/src/assets/about-us/protection.svg",
+    image: "/assets/about-us/protection.svg",
     text: "Client-Centric Approach",
   },
   
