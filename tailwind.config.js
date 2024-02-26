cfd0f090263ddf8f1f60d867326b1b2fbe0fd669
sports-fan <lovesports801@gmail.com>
2024-02-27 07:40:17 +0900

    Implement animation's druation as 2s

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,8 +44,8 @@ export const theme = {
         },
       },
       animation: {
-        slideLeft: 'slideLeft 0.5s ease-out forwards',
-        slideRight: 'slideRight 0.5s ease-out forwards'
+        slideLeft: 'slideLeft 2s ease-out forwards',
+        slideRight: 'slideRight 2s ease-out forwards'
       },
   },
 };
