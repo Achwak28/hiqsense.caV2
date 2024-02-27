624643d43974a521d518fe26b9fffe7085ade244
HarshilKhimasia <harshilkhimasia22@gmail.com>
2024-02-28 00:57:35 +0530

    Homepage path fixed for components in it.

--- src/components/pages/HomePage/Home.jsx
+++ src/components/pages/HomePage/Home.jsx
@@ -1,19 +1,22 @@
-import HeroSection from "./components/pages/HomePage/Hero/Hero";
-import Services from "./components/pages/HomePage/Services/Services";
+import HeroSection from "../HomePage/Hero/Hero";
+import Services from "../HomePage/Services/Services";
 // Sections Import End
-import WhyChooseUs from "./components/pages/HomePage/WhyChooseUs/WhyChooseUs";
-import FeaturedWork from "./components/pages/HomePage/FeaturedWork/FeaturedWork";
-import ClientTestimonials from "./components/pages/HomePage/ClientTestimonials/ClientTestimonials";
-import TimelineSection from "./components/pages/HomePage/TimelineSection/TimelineSection";
-import Awards from "./components/pages/HomePage/Awards/Awards";
-import PricingOptions from "./components/pages/HomePage/PricingOptions/PricingOptions";
-import Cta from "./components/pages/HomePage/CTA/Cta";
-import GetInTouch from "./components/pages/GetInTouch/GetInTouch";
+import WhyChooseUs from "../HomePage/WhyChooseUs/WhyChooseUs";
+import FeaturedWork from "../HomePage/FeaturedWork/FeaturedWork";
+import ClientTestimonials from "../HomePage/ClientTestimonials/ClientTestimonials";
+import TimelineSection from "../HomePage/TimelineSection/TimelineSection";
+import Awards from "../HomePage/Awards/Awards";
+import PricingOptions from "../HomePage/PricingOptions/PricingOptions";
+import Cta from "../HomePage/CTA/Cta";
+import GetInTouch from "../GetInTouch/GetInTouch";
 import { useEffect } from "react";
-import AOS from "../../../../node_modules/aos/dist/aos";
-import "aos/dist/aos.css"; 
-import "../../../../node_modules/animate.css";
- 
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "animate.css";
+// import AOS from "../../../../node_modules/aos/dist/aos";
+// import "aos/dist/aos.css";
+// import "../../../../node_modules/animate.css";
 
 export default function Home() {
   useEffect(() => {
