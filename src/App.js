e1d15e57ea5c123c1ac3c580338f0afd7e8ee9c4
Azar Obeidat <121298185+azar84@users.noreply.github.com>
2024-02-28 16:55:02 +0000

    Implemented routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import React from "react";
+
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./styles.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -5,23 +10,21 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 //import CatchAllRoute from "./components/BuilderIo";
-import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import React from "react";
 import theme from "./Theme";
-import { ThemeProvider } from "@mui/material/styles";
+//import Service from "./components/Service";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <BrowserRouter>
-          <Header />
-          # add your pages here
-          <Footer />
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/about" />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
