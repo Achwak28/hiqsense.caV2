3b64c08ae3c3f67f840b54bcf602b9834a794120
Ismail Choukri <89108710+ichoukri@users.noreply.github.com>
2024-02-29 16:17:39 +0100

    add: animate FAQ component

--- src/components/pages/ContactUs/ContactUs.jsx
+++ src/components/pages/ContactUs/ContactUs.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 import { Banner } from './components/Banner'
 import { ContactForm } from './components/Form';
@@ -9,6 +11,20 @@ import phoneIcon from '../../images/ContactUs/phoneIcon.png'
 import facebookIcon from '../../images/ContactUs/facebookIcon.png'
 
 import { useMediaQuery } from '../../../hooks/useMediaQuery';
+
+const cardVariants = {
+    offscreen: {
+        y: 300
+    },
+    onscreen: {
+        y: 50,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 1.3
+        }
+    }
+};
 
 const ContactUs = () => {
     const isDesktop = useMediaQuery('(min-width: 768px)');
@@ -55,6 +71,36 @@ const ContactUs = () => {
                     )}
                 </div>
             </Container>
+            <motion.div
+                initial="offscreen"
+                whileInView="onscreen"
+                viewport={{ once: true, amount: 0.8 }}
+                className='mt-5 px-5 h-full border-b border-white/20'
+                style={{
+                    background: `
+                        linear-gradient(0deg, rgba(255, 255, 255, 0.1) 1px, transparent 1px), 
+                        linear-gradient(90deg, rgba(255, 255, 255, 0.1) 1px, transparent 1px), 
+                        #31135E
+                    `,
+                    backgroundSize: "30px 30px",
+                }}
+            >
+                <motion.div className=" min-h-[317px] flex flex-col items-center w-full max-w-[1200px] mx-auto " variants={cardVariants}>
+                    <div className='w-full max-w-[755px] flex flex-col items-center gap-10 pt-4 pb-24 px-2 sm:px-6 text-white' >
+                        <h1 className='text-3xl sm:text-4xl md:text-5xl text-left font-bold '> Do You Have a Question? </h1>
+                        <p className='text-base font-medium'> You may go to our
+                            <span className='text-[#F6393D] font-bold px-1'>
+                                FAQ
+                            </span>
+                            page and look for the answers to your inquiries or you may send us an email.
+                        </p>
+                        <Link to='/faq' className='w-[137px] flex items-center justify-center rounded h-12 bg-[#F6393D] text-base font-bold mt-5'>
+                            View FAQ
+                        </Link>
+
+                    </div>
+                </motion.div>
+            </motion.div>
         </main>
     )
 }
