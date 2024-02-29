6fd31f0dda9e748c9bf6e85cc3ac51223ff499dd
Ismail Choukri <89108710+ichoukri@users.noreply.github.com>
2024-02-29 16:11:58 +0100

    add: the contact map

--- src/components/pages/ContactUs/ContactUs.jsx
+++ src/components/pages/ContactUs/ContactUs.jsx
@@ -8,8 +8,10 @@ import mailIcon from '../../images/ContactUs/mailIcon.png'
 import phoneIcon from '../../images/ContactUs/phoneIcon.png'
 import facebookIcon from '../../images/ContactUs/facebookIcon.png'
 
+import { useMediaQuery } from '../../../hooks/useMediaQuery';
 
 const ContactUs = () => {
+    const isDesktop = useMediaQuery('(min-width: 768px)');
 
     const ContactInfo = ({ icon, title, text }) => (
         <div className='flex gap-4 items-center h-10'>
@@ -40,6 +42,17 @@ const ContactUs = () => {
                             <ContactInfo icon={mailIcon} title='EMAIL' text="abc@gmail.com" />
                         </div>
                     </div>
+                    {isDesktop && (
+                        <div className='h-full w-full flex items-center justify-center flex-col pt-5'>
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18276991.720435414!2d-95.99999999999999!3d56!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b0d03d337cc6ad9%3A0x9968b72aa2438fa5!2sCanada!5e0!3m2!1sen!2sus!4v1709143313203!5m2!1sen!2sus"
+                                style={{ border: 0 }}
+                                allowFullScreen=""
+                                loading="lazy"
+                                className='w-full h-full'
+                            ></iframe>
+                        </div>
+                    )}
                 </div>
             </Container>
         </main>
