f437e569c2e56ffb22cb25b6fa4d1abafa4f8826
Azar Obeidat <121298185+azar84@users.noreply.github.com>
2024-03-02 20:07:16 +0000

    fixed the footer, full width for porfolio page - Do you have project in mind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
 import { useMediaQuery } from "../hooks/useMediaQuery";
 
 const NavigationLink = ({ url, label }) => (
@@ -42,34 +42,36 @@ const Footer = () => {
   ];
 
   return (
-    <footer
-      className="flex flex-col items-center px-16 w-full bg-[#31135E] py-10 max-md:px-5  max-md:max-w-full"
-    >
+    <footer className="flex flex-col items-center px-16 w-full bg-[#31135E] py-2 max-md:px-5  max-md:max-w-full">
+      <div className="z-[0] relative w-full">
+        <div className="absolute  left-1/2 p-4 rounded-lg transform -translate-x-1/2 -translate-y-1/2 w-[80%] bg-gradient-to-r from-red-600 via-purple-600 to-indigo-700">
+          <div className="block md:flex flex-col md:flex-row justify-between">
+            <div className="flex flex-col">
+              <p className="font-sans font-bold text-white text-2xl p-4 text-left">
+                Start Building your Business Today
+              </p>
+              <div className="flex flex-col md:flex-row flex-wrap gap-3 text-sm">
+                {services.map((service, index) => (
+                  <div key={index} className="flex items-center gap-1 ">
+                    <TaskAltIcon className="text-white max-w-5 max-h-5" />
+                    <span className="text-white">{service}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
 
-      <div className="flex z-10 flex-col mt-0 w-full max-w-[1188px] max-md:max-w-full">
-        <div className="w-full min-h-[150px] rounded-lg flex flex-col md:flex-row md:items-center justify-between gap-10 px-6 py-12"
-          style={{ background: "linear-gradient(122deg, rgba(206,55,87,1) 0%, rgba(86,52,168,1) 100%)" }}
-        >
-          <div className="flex flex-col gap-4 px-5">
-            <h1 className="text-2xl text-left font-bold text-white">Start Building your Business Today</h1>
-            <div className="flex flex-col md:flex-row flex-wrap gap-3 text-sm">
-              {services.map((service, index) => (
-                <div key={index} className="flex items-center gap-1 ">
-                  <TaskAltIcon className="text-white max-w-5 max-h-5" />
-                  <span className="text-white">{service}</span>
-                </div>
-              ))}
+            <div className="sm:flex gap-2 items-center justify-center w-full md:flex gap-4 items-center justify-end md:w-1/2">
+              <button className="w-1/4 bg-transparent px-2 py-2 text-white border border-white rounded-lg">
+                Get in Touch
+              </button>
+              <button className="w-1/4 bg-[#F6393D] px-2 py-2 text-white  rounded-lg">
+                About Us
+              </button>
             </div>
           </div>
-          <div className="flex gap-4">
-            <button className="h-10 rounded-md w-full sm:w-[50%] md:w-[137px] border-2 text-white text-sm font-bold">
-              Get in Touch
-            </button>
-            <button className="h-10 rounded-md w-full sm:w-[50%] md:w-[137px] bg-[#F6393D] text-white text-sm font-bold">
-              About Us
-            </button>
-          </div>
         </div>
+      </div>
+      <div className="flex z-10 flex-col mt-0 w-full max-w-[1188px] max-md:max-w-full">
         <header className="flex gap-5 justify-between mt-24 text-2xl font-bold text-white max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
           <img
             loading="lazy"
@@ -79,9 +81,7 @@ const Footer = () => {
           />
           {!isMobile && (
             <div className="flex flex-col items-center gap-2">
-              <div className="self-end mt-8" >
-                Lets do it!
-              </div>
+              <div className="self-end mt-8">Lets do it!</div>
               <div className="flex gap-4 self-end ">
                 {socialIcons.map((icon, index) => (
                   <SocialIcon key={index} {...icon} />
@@ -90,7 +90,6 @@ const Footer = () => {
             </div>
           )}
         </header>
-
 
         <nav className="flex flex-col gap-5 justify-between items-center sm:items-start  my-8 w-full max-md:flex-wrap  max-md:max-w-full">
           <div className="w-full sm:w-fit sm:gap-5 flex sm:items-start justify-between sm:justify-normal mb-5 sm:mb-10 text-white whitespace-nowrap ">
