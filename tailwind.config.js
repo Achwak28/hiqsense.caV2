8bb88bf505bb16475ce7158aa8de22c7b28719eb
sports-fan <lovesports801@gmail.com>
2024-03-04 17:21:29 -0600

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
       colors: {
         "btn-primary": "#F6393D",
         "txt-primary": "#0A0628",
+        "custom-color": "#0A0628",
+        "gradient-start": "#4334B6",
+        "gradient-end": "#F6393D",
+        'custom-purple': '#4334B6',
+        'custom-pink': '#F6393D',
+        "hero": '#372B92',
+        "content": '#100529',
+        "teamview": "#0e0327",
+        "hero-bottom-gradient": '#5f1a86',
+        "content-left-gradient": "#350736",
+        "content-underline": "#3a167e"
       },
       screens: {
         'md': '930px',
@@ -25,7 +36,23 @@ module.exports = {
       "hebrew-light": ["Open Sans Hebrew-Light"],
       "lato-bold": ["Lato-bold"],
       "lato-regular": ["Lato-regular"],
+      'opensanshebrew': ['"Open Sans Hebrew"', 'sans-serif'],
+      'ebgaramond': ['"EB Garamond"', 'serif'],
     },
+     keyframes: {
+        slideLeft: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideRight: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideLeft: 'slideLeft 2s ease-out forwards',
+        slideRight: 'slideRight 2s ease-out forwards'
+      },
   },
   plugins: [],
 };
