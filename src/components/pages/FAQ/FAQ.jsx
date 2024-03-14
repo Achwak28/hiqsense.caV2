6e1441b36bd1077d9fb9487d13ee7706fa9fecb8
Achouak Cherif <a.cherif@esi-sba.dz>
2024-03-14 05:10:28 +0100

    Add Search Functionality

--- src/components/pages/FAQ/FAQ.jsx
+++ src/components/pages/FAQ/FAQ.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import FAQHero from './FAQHero/FAQHero'
-import Questions from './Questions/Questions'
-import FAQFooter from './FAQFooter/FAQFooter'
+import React, { useState, useEffect } from "react";
+import FAQHero from "./FAQHero/FAQHero";
+import Questions from "./Questions/Questions";
+import FAQFooter from "./FAQFooter/FAQFooter";
+import { faq_answers } from "../../../constants";
 
 const FAQ = () => {
+  const [questionsList, setQuestionsList] = useState(faq_answers);
+  const [originalQuestionsList, setOriginalQuestionsList] = useState(faq_answers);
+
+  useEffect(() => {
+    setOriginalQuestionsList(faq_answers);
+  }, []);
+
+  const searchQuestion = (word) => {
+    const lowercaseWord = word.toLowerCase();
+    const filteredList = originalQuestionsList.filter((item) => {
+      const titleMatch = item.title.toLowerCase().includes(lowercaseWord);
+      const answerMatch = item.answer.toLowerCase().includes(lowercaseWord);
+      return lowercaseWord === '' || titleMatch || answerMatch;
+    });
+    setQuestionsList(filteredList);
+  };
+
   return (
     <div>
-      <FAQHero />
-      <Questions />
+      <FAQHero onSearchQuestion={searchQuestion} />
+      <Questions questions={questionsList} />
       <FAQFooter />
     </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
