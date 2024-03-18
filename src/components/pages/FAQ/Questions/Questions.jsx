76c9e2d2e9b662685412bb60e02d2c57c02c1a93
Achouak Cherif <a.cherif@esi-sba.dz>
2024-03-18 07:48:03 +0100

    Search Functionality

--- src/components/pages/FAQ/Questions/Questions.jsx
+++ src/components/pages/FAQ/Questions/Questions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Title from "./Title/Title";
 import Item from "./Item/Item";
-import { faq_sections } from "../../../../constants";
+import { faq_questions } from "../../../../constants";
 import image from "../../../../assets/FAQ/group.png";
 import "./Questions.css";
 
@@ -15,51 +15,74 @@ const Questions = (props) => {
   const [openIndex, setOpenIndex] = useState(0);
 
   const [selectedTitle, setSelectedTitle] = useState(
-    faq_sections.length > 0 ? faq_sections[0].title : null
+    faq_questions.length > 0 ? faq_questions[0].title : null
   );
 
   const handleTitleClick = (title) => {
     setSelectedTitle(title);
   };
 
+  const selectedSection = faq_questions.find(
+    (section) => section.title === selectedTitle
+  );
+
   return (
     <div className="faq-questions-container">
       <img className="left-bg hidden md:block" src={image} alt="image" />
       <div className="questions-titles">
-        <p style={titleStyle} className="font-subTitle text-center md:text-start">
+        <p
+          style={titleStyle}
+          className="font-subTitle text-center md:text-start"
+        >
           FAQ Sections
         </p>
         <div className="questions-titles-items">
-          {faq_sections.map((section) => (
-            <div
-              key={section.id}
-              className="pr-2 my-2"
-            >
+          {faq_questions.map((section) => (
+            <div key={section.id} className="pr-2 my-2">
               <Title
                 title={section.title}
                 onClick={() => handleTitleClick(section.title)}
                 isSelected={selectedTitle === section.title}
               />
             </div>
-          ))} 
+          ))}
         </div>
       </div>
       <div className="question-answer">
-        {props.questions.map((answer, index) => (
-          <div
-            key={answer.id}
-            style={{ marginTop: "1rem", marginBottom: "1rem" }}
-          >
-            <Item
-              title={answer.title}
-              answer={answer.answer}
-              index={index}
-              openIndex={openIndex}
-              setOpenIndex={setOpenIndex}
-            />
-          </div>
-        ))}
+  {props.onSearch === true ? (
+    props.questions.map((answer, index) => (
+      <div
+        key={answer.id}
+        style={{ marginTop: "1rem", marginBottom: "1rem" }}
+      >
+        <Item
+          title={answer.title}
+          answer={answer.answer}
+          index={index}
+          openIndex={openIndex}
+          setOpenIndex={setOpenIndex}
+        />
       </div>
+    ))
+  ) : (
+    selectedSection &&
+    selectedSection.questions.map((answer, index) => (
+      <div
+        key={answer.id}
+        style={{ marginTop: "1rem", marginBottom: "1rem" }}
+      >
+        <Item
+          title={answer.title}
+          answer={answer.answer}
+          index={index}
+          openIndex={openIndex}
+          setOpenIndex={setOpenIndex}
+        />
+      </div>
+    ))
+  )}
+</div>
+
     </div>
   );
 };
